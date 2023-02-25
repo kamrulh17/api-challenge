b60370e271f122211bbf317488471f26ae48d67b
kamrulh17 <kamrul17.math@gmail.com>
2023-02-26 00:20:22 +0600

    select dropdown and search button works fine

--- js/rest-countries.js
+++ js/rest-countries.js
@@ -4,6 +4,7 @@ const displayCountryName = (data, str) =>{
         if(str !== 'allCountry'){
             document.getElementById('all-countries').innerHTML = '' ;
         }
+        console.log(element) ;
 
         const div = document.createElement('div') ;
         div.innerHTML = `
@@ -14,7 +15,7 @@ const displayCountryName = (data, str) =>{
             <h4 class="h4 my-3">Country : <span class="d-inline-block bg-light">${element.name.common}</span></h4>
             <h4 class="h4 my-3">Capital : <span class="d-inline-block bg-light">${element.capital}</span></h4>
             <h5 class="h5 my-3">Populataion : <span class="d-inline-block bg-light">${element.population}</span></h5>
-            <button class="btn btn-primary my-3">Details</button>
+            <button class="btn btn-primary my-3">More Details</button>
         </div>` ;
         div.classList.add('col-lg-3')
         document.getElementById('all-countries').appendChild(div) ;
@@ -32,13 +33,36 @@ loadAllCountry() ;
 
 
 //load the searched country when search button is clicked
-const loadCountry = (searchValue) => {
+const searchByCountry = (searchValue) => {
     fetch(`https://restcountries.com/v3.1/name/${searchValue}`)
 .then(response => response.json()
 .then(data => displayCountryName(data, 'country')))
 }
+
 //search button onclick
 function searchFunction(){
     const searchValue = document.getElementById('search-box').value ;
-    loadCountry(searchValue) ;
+    searchByCountry(searchValue) ;
 }
+
+
+
+//load by region 
+function loadByRegion(region, secondParameter){
+    fetch(`https://restcountries.com/v3.1/region/${region}`)
+    .then(response => response.json())
+    .then(data => displayCountryName(data, secondParameter)) ;
+}
+
+document.getElementById('select-region').addEventListener('change', function(event){
+
+    const region = event.target.value ;
+    
+    //everytime change happens -> set the innerHTML = '' ;
+    loadByRegion(region, 'region');
+
+    if(region == 'all'){
+        loadAllCountry() ;
+    }
+    else loadByRegion(region, 'allCountry') ;
+})
